e5ae095fab08e0733ee4cac692599e57dd10873a
Rostyslav <volkov.rostyslav.8@gmail.com>
2023-11-02 20:49:55 +0100

    Implement the endpoint that shuffles a certain deck
    This endpoint shuffles a deck by game id. It requires a game id and returns 204 status code if successful, 404 status code with appropriate message if the game is not found and 400 status code with appropriate message if the deck is empty.

--- src/card-game/card-game.controller.ts
+++ src/card-game/card-game.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { CardGameService } from './card-game.service';
 import { CreateGameDto } from './dtos/create-game.dto';
 import { Game } from './interfaces/game.interface';
@@ -8,8 +8,14 @@ export class CardGameController {
   constructor(private cardGameService: CardGameService) {}
 
   @Post()
-  createGame(@Body() body: CreateGameDto): Game {
+  createGame(@Body() body: CreateGameDto): Partial<Game> {
     const { firstPlayer, secondPlayer } = body;
     return this.cardGameService.createGame(firstPlayer, secondPlayer);
+  }
+
+  @Patch('/:id/shuffle')
+  @HttpCode(204)
+  shuffleDeck(@Param('id') id: string): void {
+    this.cardGameService.shuffleDeck(id);
   }
 }
--- src/card-game/card-game.service.ts
+++ src/card-game/card-game.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Game } from './interfaces/game.interface';
 import { v4 as uuidv4 } from 'uuid';
 import { Deck } from 'src/classes/deck.class';
@@ -7,7 +11,7 @@ import { Deck } from 'src/classes/deck.class';
 export class CardGameService {
   private games: Game[] = [];
 
-  createGame(firstPlayer: string, secondPlayer: string): Game {
+  createGame(firstPlayer: string, secondPlayer: string): Partial<Game> {
     const newGame: Game = {
       id: uuidv4(),
       deck: new Deck(),
@@ -16,7 +20,23 @@ export class CardGameService {
     };
     newGame.deck.shuffle();
     this.games.push(newGame);
-    delete newGame.deck;
-    return newGame;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { deck, ...result } = newGame;
+    return result;
+  }
+
+  shuffleDeck(id: string): void {
+    const gameIndex = this.games.findIndex((game) => (game.id = id));
+
+    if (gameIndex === -1) {
+      throw new NotFoundException(`Game with ID of ${id} does not exist.`);
+    }
+    if (this.games[gameIndex].deck.count() === 0) {
+      throw new BadRequestException(
+        'The deck is empty and cannot be shuffled.',
+      );
+    }
+
+    this.games[gameIndex].deck.shuffle();
   }
 }
